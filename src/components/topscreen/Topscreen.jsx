698993233f98148c4b6666e3cf550d09c92a06cf
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-01-19 14:44:20 +0600

    finish top-screen

--- src/components/topscreen/Topscreen.jsx
+++ src/components/topscreen/Topscreen.jsx
@@ -9,22 +9,43 @@ import { title } from "../../data/app";
 function Topscreen() {
   return (
     <div className="top-screen">
-      <Container>
-        <header className="top-screen-header">
-          <a href="#">
-            <Logo />
-          </a>
+      <div className="top-screen-top">
+        <Container>
+          <header className="top-screen-header">
+            <a href="#">
+              <Logo />
+            </a>
 
-          <nav className="nav">
-            {menuItems.map((item) => (
-              <MenuLink label={item.label} url={item.url} key={item.label} />
-            ))}
-          </nav>
-        </header>
-        <div className="top-screen-inner">
+            <nav className="nav">
+              {menuItems.map((item) => (
+                <MenuLink label={item.label} url={item.url} key={item.label} />
+              ))}
+            </nav>
+          </header>
+        </Container>
+      </div>
+      <div className="top-screen-content">
+        <Container>
           <Title name={title.name} role={title.role} />
-        </div>
-      </Container>
+        </Container>
+      </div>
+      <div className="top-screen-bottom">
+        <a className="scroll-to" href="#about">
+          <span>дивитися роботи</span>
+          <svg
+            width="37"
+            height="22"
+            viewBox="0 0 37 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M18.4999 21.091C17.8368 21.091 17.1738 20.8378 16.6682 20.3325L0.759023 4.4232C-0.253008 3.41117 -0.253008 1.77034 0.759023 0.758716C1.77064 -0.252905 3.41115 -0.252905 4.42326 0.758716L18.4999 14.8362L32.5766 0.759208C33.5886 -0.252414 35.229 -0.252414 36.2405 0.759208C37.253 1.77083 37.253 3.41166 36.2405 4.42369L20.3316 20.333C19.8258 20.8384 19.1628 21.091 18.4999 21.091Z"
+              fill="white"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
